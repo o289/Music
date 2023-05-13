d7aa14e7f452a820d1eb7c722b08183bb9b06c52
o289 <nanyouda11@gmail.com>
2023-05-13 11:12:46 +0900

    remove

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ class MusicPlay{
             stopping.classList.add('prepare')
             Now.classList.add('prepare')
             submit_loop.classList.add('none')
-            id.classList.add('prepare');
             tab.classList.add('none')
         };
         function remove(){
